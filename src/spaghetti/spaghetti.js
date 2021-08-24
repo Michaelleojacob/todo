dbcee6d90caaec48783a03221343528bc121dd52
Michael <michaelleojacob@gmail.com>
2021-08-24 16:54:30 -0700

    fixed logic to close other new item displays when a different item display is clicked to display. Things look like they are working as intended for now.

--- src/spaghetti/spaghetti.js
+++ src/spaghetti/spaghetti.js
@@ -5,16 +5,18 @@ export default function myListenForNewBtnClick() {
 		giveEachNewItemAnEvent() {
 			document.addEventListener('click', e => {
 				if (e.target.classList.contains('newBtnWrap')) {
-					newBtnClick.closeAnyActiveForms();
-					e.target.parentNode.childNodes[1].classList.toggle('formInactive');
+					const myNodeList = document.querySelectorAll('.formwrap');
+					for (let item of myNodeList) {
+						if (item === e.target.parentNode.childNodes[1]) {
+							e.target.parentNode.childNodes[1].classList.toggle(
+								'formInactive'
+							);
+						} else {
+							item.classList.add('formInactive');
+						}
+					}
 				}
 			});
-		},
-		closeAnyActiveForms() {
-			const myNodeList = document.querySelectorAll('.formwrap');
-			for (let item of myNodeList) {
-				item.classList.add('formInactive');
-			}
 		},
 	};
 	document.addEventListener('DOMContentLoaded', () =>
