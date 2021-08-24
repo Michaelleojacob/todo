90d8c8f93361d7d85f473d44eaa4063673d777fb
Michael <michaelleojacob@gmail.com>
2021-08-24 15:19:01 -0700

    added all the styling for how I want the add new task display to be

--- src/newButtonFactory/renderNewButton.js
+++ src/newButtonFactory/renderNewButton.js
@@ -4,14 +4,53 @@ export default function myRenderNewButton({ id, text, parent }) {
 	const renderNewButton = {
 		makeAddNewButton() {
 			this.parentNewButtonWrap = document.createElement('div');
-			this.parentNewButtonWrap.classList.add('parentNewButtonWrap');
+			this.parentNewButtonWrap.classList.add(
+				`${id}Parent`,
+				'parentNewButtonWrap'
+			);
 			this.newItemBtn = document.createElement('button');
 			this.newItemBtn.classList.add('newBtnWrap');
 			this.newItemBtn.setAttribute('id', id);
 			this.newItemBtn.innerHTML = `<i class="plusIcon fas fa-plus"></i> <span class="new${text}">${text}</span>`;
 			parent.appendChild(this.parentNewButtonWrap);
 			this.parentNewButtonWrap.appendChild(this.newItemBtn);
+			this.makeNewItemDisplay(id);
 			return this.newItemBtn;
+		},
+		makeNewItemDisplay(id) {
+			this.formwrap = document.createElement('div');
+			this.formwrap.classList.add('formwrap');
+
+			this.form = document.createElement('form');
+			this.form.setAttribute('id', `${id}Form`);
+
+			this.inputWrap = document.createElement('div');
+			this.inputWrap.classList.add('inputWrap');
+
+			this.inputArea = document.createElement('input');
+			this.inputArea.classList.add('inputArea');
+			this.inputArea.placeholder = `enter a new task here`;
+			this.inputArea.setAttribute('id', `${id}InputArea`);
+
+			this.submitAndCancelWrap = document.createElement('div');
+			this.submitAndCancelWrap.classList.add('submitAndCancelWrap');
+
+			this.submitBtn = document.createElement('button');
+			this.submitBtn.classList.add('submitBtn');
+			this.submitBtn.textContent = `Add`;
+			this.submitBtn.setAttribute('id', `${id}Submit`);
+
+			this.cancelBtn = document.createElement('button');
+			this.cancelBtn.classList.add('cancelBtn');
+			this.cancelBtn.textContent = 'Cancel';
+			this.cancelBtn.setAttribute('id', `${id}Cancel`);
+
+			this.inputWrap.appendChild(this.inputArea);
+			this.submitAndCancelWrap.appendChild(this.submitBtn);
+			this.submitAndCancelWrap.appendChild(this.cancelBtn);
+			this.formwrap.appendChild(this.inputWrap);
+			this.formwrap.appendChild(this.submitAndCancelWrap);
+			this.parentNewButtonWrap.appendChild(this.formwrap);
 		},
 	};
 	return renderNewButton.makeAddNewButton();
--- src/spaghetti/spaghetti.js
+++ src/spaghetti/spaghetti.js
@@ -3,27 +3,17 @@ import './spaghetti.css';
 export default function myListenForNewBtnClick() {
 	const newBtnClick = {
 		init() {
-			this.listenForEvent();
+			// this.listenForEvent();
 		},
-		cacheDom() {
-			this.newTask = document.querySelector('#newTask');
-			this.newProjTask = document.querySelector('#newProjTask');
-			this.newProject = document.querySelector('#newProject');
-		},
-		listenForEvent() {
-			document.addEventListener('click', this.handleEventDeligation);
-		},
-		handleEventDeligation(event) {
-			if (event.target === document.querySelector('#newTask')) {
-				console.log('hi');
-			}
-			if (event.target === document.querySelector('#newProjTask')) {
-				console.log('hey');
-			}
-			if (event.target === document.querySelector('#newProject')) {
-				console.log('hello there');
-			}
-		},
+		// cacheDom() {
+		// 	this.newTask = document.querySelector('#newTask');
+		// 	this.newProjTask = document.querySelector('#newProjTask');
+		// 	this.newProject = document.querySelector('#newProject');
+		// },
+		// listenForEvent() {
+		// 	document.addEventListener('click', this.handleEventDeligation);
+		// },
+		// handleEventDeligation(event) {},
 	};
 	newBtnClick.init();
 }
