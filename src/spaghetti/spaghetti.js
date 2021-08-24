6b330ef6ef4ed6eaac627bc6f488e8385e6eb465
Michael <michaelleojacob@gmail.com>
2021-08-24 15:54:27 -0700

    got all the new item buttons working and their displays to toggle.

--- src/spaghetti/spaghetti.js
+++ src/spaghetti/spaghetti.js
@@ -3,13 +3,19 @@ import './spaghetti.css';
 export default function myListenForNewBtnClick() {
 	const newBtnClick = {
 		init() {
-			document.addEventListener('DOMContentLoaded', () => {
-				const myElement = document.getElementById('newTask');
-				myElement.addEventListener('click', e => {
+			// const myElement = document.getElementById('newTask');
+			// myElement.addEventListener('click', e => {
+			// 	e.target.parentNode.childNodes[1].classList.toggle('formInactive');
+			// });
+		},
+		initTwo() {
+			document.addEventListener('click', e => {
+				console.log(e.target);
+				if (e.target.classList.contains('newBtnWrap')) {
 					e.target.parentNode.childNodes[1].classList.toggle('formInactive');
-				});
+				}
 			});
 		},
 	};
-	newBtnClick.init();
+	document.addEventListener('DOMContentLoaded', () => newBtnClick.initTwo());
 }
